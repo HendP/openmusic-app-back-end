876bff8bcf9853b33f183fe1382e39dd821f7432
HendP <hendpermana12@gmail.com>
2022-10-20 14:35:04 +0700

    setup server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,80 @@
-console.log('Hallo kita akan membuat RESTful API');
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const AlbumsValidator = require('./validator/albums');
+const SongsValidator = require('./validator/songs');
+const AlbumService = require('./services/openmusic/AlbumsService');
+const SongsService = require('./services/openmusic/SongsService');
+const ClientError = require('./exceptions/ClientError');
+
+const init = async () => {
+  const albumsService = new AlbumService();
+  const songsService = new SongsService();
+
+  const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return response.continue || response;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
